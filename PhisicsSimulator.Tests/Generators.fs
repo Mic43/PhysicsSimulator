91ccff92981229ad00b2bbd4e4b94786bff2bc61
Michal <m.wudarczyk@hotmail.com>
2024-09-10 15:24:31 +0200

    New unit tests and generators for spatial tree structure

--- PhisicsSimulator.Tests/Generators.fs
+++ PhisicsSimulator.Tests/Generators.fs
@@ -1,15 +1,54 @@
 module PhisicsSimulator.Tests.Generators
 
+open System
 open FSharpPlus.Data
 open FsCheck
 open PhysicsSimulator.Utilities
 
+module Common =
+    let genFloatInRange (min, max) =
+        gen {
+            let! n = Arb.generate<PositiveInt>
+            let f = float n.Get / float Int32.MaxValue
+            return min + (f * (max - min))
+        }
+
+    let properRangeGen =
+        gen {
+            let! l = Arb.generate<NormalFloat> |> Gen.map (_.Get)
+
+            return! (l, Single.MaxValue |> float) |> genFloatInRange |> Gen.map (fun h -> (l, h))
+        }
+
 module SpatialTree =
     let emptyTree<'T> =
-        (Arb.generate<PositiveInt> |> Gen.two,
-         Arb.Default.NormalFloat().Generator
-         |> Gen.map (_.Get)
-         |> Gen.two
-         |> Gen.nonEmptyListOf)
+        (Arb.generate<PositiveInt> |> Gen.two, Common.properRangeGen |> Gen.nonEmptyListOf )
         ||> Gen.map2 (fun (maxLeafObjects, maxDepth) boundaries ->
-            (maxLeafObjects.Get, maxDepth.Get, boundaries) |||> SpatialTree.init)
+            (maxLeafObjects.Get, maxDepth.Get, boundaries) |||> SpatialTree.init<'T>)
+
+    let singleNodeTree<'T> (singleNodeGen: Gen<'T>) =
+        gen {
+            let! empty = emptyTree<'T>
+            let! newRoot = singleNodeGen |> Gen.listOfLength empty.MaxLeafObjects |> Gen.map Leaf
+            return { empty with Root = newRoot }
+        }
+
+    let extentInBoundary (boundary: float * float) =
+        if boundary |> fst > (boundary |> snd) then
+            invalidArg "boundary" "invalid boundary"
+
+        gen {
+            let! pos = boundary |> Common.genFloatInRange
+            let! pos2 = (pos, boundary |> snd) |> Common.genFloatInRange
+
+            return (pos, pos2 - pos)
+        }
+
+    let extentInBoundaries (boundary: (float * float) list) =
+        boundary |> List.map extentInBoundary |> Gen.sequence
+
+    let extent dimension =
+        gen {
+            let! b = Common.properRangeGen |> Gen.listOfLength dimension
+            return! b |> extentInBoundaries
+        }
--- PhisicsSimulator.Tests/UtilitiesTests.fs
+++ PhisicsSimulator.Tests/UtilitiesTests.fs
@@ -6,11 +6,12 @@ open FsCheck
 open FsCheck.Xunit
 open PhysicsSimulator.Utilities
 
-
 let (.=.) left right =
     left = right |@ sprintf "%A = %A" left right
 
 module SpatialTree =
+
+    open PhisicsSimulator.Tests
 
     [<Property>]
     let ``init called with empty boundaries throws exception`` (maxLeafObjects: PositiveInt) (maxDepth: PositiveInt) =
@@ -38,4 +39,51 @@ module SpatialTree =
             let actual = tree |> SpatialTree.getObjectBuckets
             let expected = Seq.empty
             actual .=. expected)
-        |> Prop.forAll (PhisicsSimulator.Tests.Generators.SpatialTree.emptyTree |> Arb.fromGen)
+        |> Prop.forAll (Generators.SpatialTree.emptyTree |> Arb.fromGen)
+
+    [<Property>]
+    let ``For single object tree getObjectBuckets returns object's singleton `` (object: int) =
+
+        let treeGen =
+            gen {
+                let! emptyTree = Generators.SpatialTree.emptyTree<int>
+
+                let! extent = emptyTree.SpaceBoundaries.Length |> Generators.SpatialTree.extent
+
+                return
+                    object
+                    |> SpatialTree.insert emptyTree (fun _ ->
+                        extent |> List.map (fun (p, s) -> {| Position = p; Size = s |}))
+            }
+
+        (fun tree ->
+            let actual = tree |> SpatialTree.getObjectBuckets
+            let expected = object |> List.singleton |> Seq.singleton
+
+            actual |> Seq.toList .=. (expected |> Seq.toList))
+        |> Prop.forAll (treeGen |> Arb.fromGen)
+
+    [<Property(EndSize = 10)>]
+    let ``For tree with full Leaf Root node inserting objects causes node to split if maxDepth is not reached`` object =
+        let treeGen =
+            Arb.generate<int>
+            |> Generators.SpatialTree.singleNodeTree
+            |> Gen.filter (fun tree -> tree.MaxDepth > 1)
+
+        (fun tree ->
+
+            // all objects are placed in the plane origin
+            let actual =
+                object
+                |> SpatialTree.insert tree (fun _ ->
+                    tree.SpaceBoundaries |> List.map (fun (p, _) -> {| Position = p; Size = 0.0 |}))
+
+            match actual.Root with
+            | Leaf _ -> false
+            | NonLeaf root when root.Length > 0 ->
+                match root[0] with
+                | Leaf l when l.Length = tree.MaxLeafObjects + 1 -> true
+                | Leaf _ -> false
+                | NonLeaf _ -> false
+            | _ -> false)
+        |> Prop.forAll (treeGen |> Arb.fromGen)
